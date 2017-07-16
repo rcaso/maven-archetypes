17d83562192787eb8dcce5c7db7779fc076351a7
raul <raul@raul-QOSMIO-F755>
2017-07-16 10:52:10 -0500

    new component shava-core-keycloak archetype

---
+++ shava-backendapp-keycloak-archetype/src/main/resources/archetype-resources/src/main/java/configuration/restful/JAXRSConfiguration.java
@@ -0,0 +1,20 @@
+package ${package}.configuration.restful;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+/**
+ * <ul>
+ * <li>Copyright 2017 Shava. Todos los derechos reservados.</li>
+ * </ul> 
+ * 
+ * La Class JAXRSConfiguration.
+ * 
+ * @author RCASO
+ * @version 1.0 , 29/04/2017
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+}
